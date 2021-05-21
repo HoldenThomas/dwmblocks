681b6a8407a7c924f195114429a34e5a058cd604
HCThomas <hthomas1215@gmail.com>
2021-05-21 00:35:02 -0400

    first commit

---
+++ blocks.def.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", 		"sb-forecast",		10000,			0},
+	{"", 		"sb-cpu",		2,			0},
+	{"", 		"sb-memory",		2,			0},
+	{"", 		"sb-nettraf",		1,			0},
+	{"", 		"sb-internet",		1,			0},
+	{"", 		"sb-battery",		60,			0},
+	{"", 		"sb-volume",		0,			10},
+	{"", 		"sb-clock",		60,			0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
+
