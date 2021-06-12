3fcde0152dc3f55534bdd19cc1e6b0743919c586
HCThomas <hthomas1215@gmail.com>
2021-06-12 00:47:00 -0400

    change active blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 		"sb-forecast",		10000,			0},
-	{"", 		"sb-cpu",		2,			0},
-	{"", 		"sb-memory",		2,			0},
-	{"", 		"sb-nettraf",		1,			0},
+//	{"", 		"sb-forecast",		10000,			0},
+//	{"", 		"sb-cpu",		2,			0},
+//	{"", 		"sb-memory",		2,			0},
+//	{"", 		"sb-nettraf",		1,			0},
 	{"", 		"sb-internet",		1,			0},
 	{"", 		"sb-battery",		60,			0},
 	{"", 		"sb-volume",		0,			10},
