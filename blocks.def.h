7e40a959ac4fdae863c48d8aa256c446e10d9dfb
holden <spiritupload@protonmail.com>
2023-04-27 16:32:55 -0400

    update blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    //{"", 		"sb-forecast.sh",		10000,			0},
+	{"", 		"sb-memory.sh",		  5,			0},
+	{"", 		"sb-cpu.sh",		    5,			0},
+	{"", 		"sb-nettraf.sh",		1,			0},
+	{"", 		"sb-internet.sh",		5,			0},
+	{"", 		"sb-battery.sh",		60,			0},
+	{"", 		"sb-volume.sh",		  0,			10},
+	{"", 		"sb-clock.sh",		  60,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
